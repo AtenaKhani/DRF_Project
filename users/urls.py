b831bef73a81baa66c9e0b7ecd4d724555960b48
Atena <atenakhani904@gmail.com>
2024-09-07 16:53:48 +0330

    Added URLs for logout, password change, and resend email verification to the project

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,5 @@
+from dj_rest_auth.registration.views import ResendEmailVerificationView
+from dj_rest_auth.views import LogoutView, PasswordChangeView
 from django.urls import path, include, re_path
 from .views import  CustomRegisterView,CustomVerifyEmailView,CustomLoginView,UserProfileView
 urlpatterns =[
@@ -5,6 +7,9 @@ urlpatterns =[
    path('verify-email/', CustomVerifyEmailView.as_view(), name='rest_verify_email'),
    path('login/', CustomLoginView.as_view(), name='login'),
    path('profile/', UserProfileView.as_view(), name='profile'),
+   path('logout/', LogoutView.as_view(), name='logout'),
+   path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+   path('resend-email/', ResendEmailVerificationView.as_view(), name="rest_resend_email"),
     re_path(
         r'^account-confirm-email/(?P<key>[-:\w]+)/$', CustomVerifyEmailView.as_view(),
         name='account_confirm_email',
