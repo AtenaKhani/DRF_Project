6e6235ea89ef1f47f2ea3cdd62b29ce0cc8012e7
Atena <atenakhani904@gmail.com>
2024-09-08 13:54:49 +0330

    Add premium adand change create and update methods of ads

--- ads/serializers.py
+++ ads/serializers.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import random
 import string
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.reverse import reverse
 from .models import Car,Ad
 from datetime import datetime
@@ -54,10 +55,16 @@ class AdCreateSerializer(serializers.ModelSerializer):
     car=CarSerializer()
     class Meta :
         model = Ad
-        fields = ['car','description','location','price','payment_method']
+        fields = ['car','description','location','price','payment_method','type']
     def validate_price(self, value):
         if value < 0:
             raise serializers.ValidationError("Price cannot be negative.")
+        return value
+    def validate_type(self,value):
+        if value == 'premium':
+            user = self.context['request'].user
+            if user.wallet_balance < 50000:
+                raise  serializers.ValidationError("Your wallet balance is less than 50000 Tomans, you cannot register a premium ad")
         return value
 
     def generate_ad_code(self):
@@ -73,11 +80,20 @@ class AdCreateSerializer(serializers.ModelSerializer):
             validated_data['seller_contact'] = user.phone_number
             car = Car.objects.create(**car_data)
             ad = Ad.objects.create(**validated_data, car=car,user=user)
+            if ad.type == 'premium':
+                user.wallet_balance -= 50000
+                user.save()
             ad.url = reverse('ad_detail', kwargs={'pk': ad.pk})
             ad.save()
             return ad
 
     def update(self, instance, validated_data):
+        if instance.type == 'free' and validated_data['type']=='premium':
+            user = self.context['request'].user
+            user.wallet_balance -= 50000
+            user.save()
+        if instance.type == 'premium' and validated_data['type']=='free':
+            raise serializers.ValidationError("You cannot change the premium ad to free")
         car_data = validated_data.pop('car', None)
         if car_data:
             for attr, value in car_data.items():
